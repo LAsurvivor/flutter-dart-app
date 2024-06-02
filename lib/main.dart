959549e30ccd10993843ed9254cccb44267973f1
Zhaohan Hu <lasurvivor1787@gmail.com>
2024-06-02 15:17:34 -0700

    initial setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,29 @@
-import 'package:flutter/cupertino.dart';
+import 'package:first_app/categories.dart';
+import 'package:flutter/material.dart';
 
-import 'package:first_app/gradient_container.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+final theme = ThemeData(
+  useMaterial3: true,
+  colorScheme: ColorScheme.fromSeed(
+    brightness: Brightness.dark,
+    seedColor: const Color.fromARGB(255, 131, 57, 0),
+  ),
+  textTheme: GoogleFonts.latoTextTheme(),
+);
 
 void main() {
-  runApp(
-    const MyApp(),
-  );
+  runApp(const App());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({
-    super.key,
-  });
+class App extends StatelessWidget {
+  const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const CupertinoApp(
-      home: CupertinoPageScaffold(
-        child: GradientContainer.purple(),
-      ),
+    return MaterialApp(
+        theme: theme,
+        home: const CategoriesScreen(),
     );
   }
 }
